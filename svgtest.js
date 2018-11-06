a695d5f3b6c9ab82dc87b163b4f9fbd168f2a612
rebe4175 <rebe4175@stud.kea.dk>
2018-11-06 12:58:25 +0100

    svg test 1

--- svgtest.js
+++ svgtest.js
@@ -4,10 +4,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
 async function svgProgram() {
 
+    let selected;
+    let selectedID;
+    let color;
+    let active;
+
 
     // load svg
 
-    let mySvg = await fetch("husetager.svg");
+    let mySvg = await fetch("husigen.svg");
     let svg = await mySvg.text();
 
 
@@ -16,5 +21,18 @@ async function svgProgram() {
 
     // select objekt
 
+    document.querySelector("#house").addEventListener("click", clicked);
 
-}
+    function clicked() {
+        console.log(event.target);
+
+        selected = event.target;
+
+        selectedID = selected.getAttribute("id");
+
+        color = selected.getAttribute("fill");
+
+        document.querySelector("#" + selectedID).setAttribute("fill", "#d3072A");
+    }
+
+};
