2e8e92e628e3844cc84506b526f92e3c50227800
rebe4175 <rebe4175@stud.kea.dk>
2018-11-08 09:24:47 +0100

    svg almost done (mangler links til nye sider)

--- svgtest.js
+++ svgtest.js
@@ -4,35 +4,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
 async function svgProgram() {
 
-    let selected;
-    let selectedID;
-    let color;
-    let active;
 
-
-    // load svg
-
-    let mySvg = await fetch("husigen.svg");
+    let mySvg = await fetch("husetagernavne.svg");
     let svg = await mySvg.text();
 
-
     document.querySelector("#house").innerHTML = svg;
-
-
-    // select objekt
-
-    document.querySelector("#house").addEventListener("click", clicked);
-
-    function clicked() {
-        console.log(event.target);
-
-        selected = event.target;
-
-        selectedID = selected.getAttribute("id");
-
-        color = selected.getAttribute("fill");
-
-        document.querySelector("#" + selectedID).setAttribute("fill", "#d3072A");
-    }
 
 };
